49a5bc021b85d0209640b7b68d5831e08d3ebf79
espkiran <32625636+espkiran@users.noreply.github.com>
2025-01-09 06:31:47 +0545

    upload 2025 growmore data

---
+++ grmrscript.js
@@ -0,0 +1,454 @@
+function createNepaliDropdown(id, options) {
+  const select = document.getElementById(id);
+
+  // Assuming options is an array of strings
+  options.forEach(option => {
+    const optionElement = document.createElement('option');
+    optionElement.textContent = option;
+    select.appendChild(optionElement);
+  });
+}
+
+// Define options for each dropdown
+const loanWorkOptions = ['छान्नुहोस', '... ... ...', 'ब्यक्तिगत', 'ब्यापार', 'आकस्मिक', 'साना ब्यावसायी', 'ब्यक्तिगत जमानी', 'कृषि', 'भेहिकल'];
+const ltypeOptions = ['छान्नुहोस', '... ... ...', 'ब्यक्तिगत', 'ब्यापार', 'आकस्मिक', 'साना ब्यावसायी', 'ब्यक्तिगत जमानी', 'कृषि', 'भेहिकल'];
+const pogapaAOptions = ['छान्नुहोस', '... ... ...', 'गा.पा.', 'न.पा.', 'म.न.पा.', 'उप.म.न.पा.'];
+const pogapaOptions = ['छान्नुहोस', '... ... ...', 'गा.पा.', 'न.पा.', 'म.न.पा.', 'उप.म.न.पा.'];
+const togapaz1Options = ['छान्नुहोस', '... ... ...', 'गा.पा.', 'न.पा.', 'म.न.पा.', 'उप.म.न.पा.'];
+const togapaz5Options = ['छान्नुहोस', '... ... ...', 'गा.पा.', 'न.पा.', 'म.न.पा.', 'उप.म.न.पा.'];
+const togapaz6Options = ['छान्नुहोस', '... ... ...', 'गा.पा.', 'न.पा.', 'म.न.पा.', 'उप.म.न.पा.'];
+const togapa1Options = ['छान्नुहोस', '... ... ...', 'गा.पा.', 'न.पा.', 'म.न.पा.', 'उप.म.न.पा.'];
+const togapa5Options = ['छान्नुहोस', '... ... ...', 'गा.पा.', 'न.पा.', 'म.न.पा.', 'उप.म.न.पा.'];
+const togapa6Options = ['छान्नुहोस', '... ... ...', 'गा.पा.', 'न.पा.', 'म.न.पा.', 'उप.म.न.पा.'];
+
+
+
+
+
+const mpdtpOptions = ['छान्नुहोस', '... ... ...', "दैनिक", "मासिक", "त्रैमासिक", "एकमुष्ट", "मासिक ब्याज भुक्तानी"];
+
+
+// Create dropdowns
+createNepaliDropdown('loanWork', loanWorkOptions);
+createNepaliDropdown('ltype', ltypeOptions);
+createNepaliDropdown('pogapaA', pogapaAOptions);
+createNepaliDropdown('pogapa', pogapaOptions);
+createNepaliDropdown('mperiodtype', mpdtpOptions);
+createNepaliDropdown('togapaz1', togapaz1Options);
+createNepaliDropdown('togapaz5', togapaz5Options);
+createNepaliDropdown('togapaz6', togapaz6Options);
+createNepaliDropdown('togapa1', togapa1Options);
+createNepaliDropdown('togapa5', togapa5Options);
+createNepaliDropdown('togapa6', togapa6Options);
+
+
+function createDropdown(id, start, end) {
+  const select = document.getElementById(id);
+
+  for (let i = start; i <= end; i++) {
+    const option = document.createElement('option');
+    option.value = i;
+    option.textContent = convertToDevanagari(i);
+    select.appendChild(option);
+  }
+}
+
+function convertToDevanagari(number) {
+  const devanagariNumerals = ['०', '१', '२', '३', '४', '५', '६', '७', '८', '९'];
+  const numberStr = number.toString();
+  let devanagariStr = '';
+  for (let i = 0; i < numberStr.length; i++) {
+    const digit = numberStr[i];
+    devanagariStr += devanagariNumerals[digit];
+  }
+  return devanagariStr;
+}
+
+createDropdown('pward', 1, 32); 
+createDropdown('tward', 1, 32); 
+createDropdown('mperiodtime', 1, 32); 
+createDropdown('housefloor', 1, 32); 
+createDropdown('togapaz2', 1, 32); 
+createDropdown('togapaz3', 1, 32); 
+createDropdown('togapaz4', 1, 32); 
+createDropdown('togapa2', 1, 32); 
+createDropdown('togapa3', 1, 32); 
+createDropdown('togapa4', 1, 32); 
+createDropdown('myage', 16, 80);
+
+
+function convertToWords(number){
+    var str = number.toString();
+    var length = str.length;
+    var nepaliStr = [];
+    if(length > 3){
+
+       // get last three digits of given number 
+       var lastThree = str.lastThree();
+
+       // remove last three digit and take remaining digits
+       var remStr = str.removeLastThree();
+
+       // get length of remaining digits in number
+       var remStrLength = remStr.length;
+
+       // make a array 
+       var remStrips = lastThree.sliceToTwo().concat(remStr.sliceToTwo());
+       
+       console.log(remStrips)
+       //reverse the array and join
+       nepaliStr = remStrips.reverseAndJoin();
+   }
+   else{
+
+     remStrips = str.sliceToTwo();
+     nepaliStr = remStrips.reverseAndJoin();
+}
+
+return nepaliStr;
+
+}
+
+function convertToNepaliDigit(number){
+   var number = number.toString();
+   var sliced = [];
+   var numberLength = number.length
+   var nepali_digits = ['०','१','२','३','४','५','६','७','८','९' ];
+       for(i=0; i< numberLength; i++){
+       sliced.push(nepali_digits[number.substr(number.length - 1)]);
+       number = number.slice(0,-1);
+   }
+   return sliced.reverse().join('').toString();
+}
+
+function convertToNepaliText(number){
+   var number = number.toString();
+   var number_before_decimal = number.split(".")[0]
+   var number_after_decimal = number.split(".")[1]
+   var text1 = convertToWords(number_before_decimal);
+   var text2 = "";
+   if(typeof number_after_decimal !== "undefined"){
+       text2 = convertToWords(number_after_decimal);
+       return text1 + " दशमलव " + text2;
+   }
+   else{
+       return text1;
+   }
+
+}
+
+function convertToNepaliNumber(number){
+   var number = number.toString();
+   var number_before_decimal = number.split(".")[0]
+   var number_after_decimal = number.split(".")[1]
+   var text1 = convertToNepaliDigit(number_before_decimal);
+   var text2 = "";
+   if(typeof number_after_decimal !== "undefined"){
+       text2 = convertToNepaliDigit(number_after_decimal);
+       return text1 + "." + text2;
+   }
+   else{
+       return text1;
+   }
+
+}
+
+
+function convertToCommaNumber(number){
+   var number = number.toString();
+   var number_before_decimal = number.split(".")[0]
+   var number_after_decimal = number.split(".")[1]
+   var text1 = commafy(number_before_decimal);
+   var text2 = "";
+   if(typeof number_after_decimal !== "undefined"){
+       text2 =  number_after_decimal;
+       return text1 + "<b>।</b>" + text2;
+   }
+   else{
+       return text1;
+   }
+
+}
+
+function commafy(number){
+
+var str = number.toString();
+    var length = str.length;
+    if(length > 3){
+
+       // get last three digits of given number 
+       var lastThree = str.lastThree();
+
+       // remove last three digit and take remaining digits
+       var number = str.slice(0, -3);
+       var sliced = [];
+       var numberLength = number.length
+           for(i=0; 1 <= number.length; i++){
+             sliced.push(number.substr(number.length - 2));
+             number = number.slice(0,-2);
+           }
+       
+       // make a array 
+       var remStrips = sliced.reverse().join(",")+","+lastThree;
+       return remStrips;
+   }
+   else{
+
+     return str;
+}
+
+}
+
+
+String.prototype.lastThree = function(){
+	return this.substr(this.length - 3);
+};
+String.prototype.sliceToTwo = function(){
+	var sliced = [];
+	var number = this;
+	var numberLength = number.length
+		for(i=0; 1 <= number.length; i++){
+          sliced.push(parseInt(number.substr(number.length - 2)).toString());
+          number = number.slice(0,-2);
+        }
+	return sliced;
+}
+String.prototype.removeLastThree = function(){
+	return this.slice(0,-3);
+};
+
+String.prototype.stringifyValues = function(){
+	var digits = ['', 'एक', 'दुई' ,'तीन', 'चार', 'पाँच' , 'छ', 'सात' , 'आठ' , 'नौ' , 'दश', 'एघार', 'बाह्र ', 
+	'तेह्र', 'चौध', 'पन्ध्र', 'शोह्र', 'सत्र', 'अठार', 'उन्नाइस', 'बीस', 'एक्काइस', 'बाइस', 'तेइस', 'चौबीस', 'पच्चीस', 
+	'छब्बिस', 'सत्ताइस', 'अठ्ठाइस', 'उन्नतीस', 'तीस', 'एकतीस', 'बत्तीस', 'तेत्तिस', 'चौतीस', 'पैँतीस', 'छत्तिस', 'सैँतीस', 
+	'अठ्तीस', 'उन्नचालीस', 'चालीस', 'एकचालीस', 'बयालीस', 'त्रिचालिस', 'चौवालीस', 'पैँतालीस', 'छयालीस', 'सतचालिस', 
+	'अठचालीस', 'उन्नपचास', 'पचास', 'एकाउन्न', 'बाउन्न', 'त्रिपन्न', 'चौवन्न', 'पचपन्न', 'छपन्न', 'सन्ताउन्न', 'अन्ठाउन्न', 
+	'उन्नसाठी', 'साठी', 'एकसठ्ठी', 'बैसठ्ठी', 'त्रिसठ्ठी', 'चौसठ्ठी', 'पैसठ्ठी', 'छैसठ्ठी', 'सतसठ्ठी', 'अठसठ्ठी', 'उनान्सत्तरी', 
+	'सत्तरी', 'एकहत्तर', 'बहत्तर', 'तिरहत्तर', 'चौरहत्तर', 'पचहत्तर', 'छहत्तर', 'सतहत्तर', 'अठहत्तर', 'उनानअसी', 'असी', 
+	 'एकासी', 'बयासी', 'त्रियासी', 'चौरासी', 'पचासी', 'छयासी', 'सतासी', 'अठासी', 'उनान्नब्बे', 'नब्बे', 'एकानब्बे',
+	  'बयानब्बे', 'त्रियानब्बे', 'चौरानब्बे', 'पन्चानब्बे', 'छयानब्बे', 'सन्तानब्बे', 'अन्ठानब्बे', 'उनान्सय', 'सय' ];
+	return digits[this];
+
+};
+
+
+String.prototype.stringifyPlace = function(){
+	var place = [ '', 'सय','हजार', 'लाख', 'करोड', 'अरब', 'खरब', 'निल', 'पद्म', 'संख्य' ];
+	return place[this];
+};
+
+Array.prototype.reverseAndJoin = function(){
+	 var list = [];
+	 this.forEach(function(value, i){
+ 		if(parseInt(value) == 0){
+ 			list.push('');}
+ 		else if( i < 1){
+			list.push(value.stringifyValues());
+ 		}
+ 		else{
+ 			list.push(value.stringifyValues()+" "+ i.toString().stringifyPlace());
+ 		}
+ 	});
+    return list.reverse().join(" ");
+}
+document.getElementById("convert").addEventListener("click", convertIt);
+
+function convertIt() {
+  // List of input IDs to process
+  var inputIds = ["Qno", "Qmob", "Lper"];
+  
+  // Loop through each input ID
+  inputIds.forEach(function(inputId) {
+    var input = document.getElementById(inputId).value;
+    var outputIds = getOutputIds(inputId);
+    var output = convertToNepaliNumber(input);
+    // var output = input; // Just echoing the input for demonstration purposes
+
+    // Update each output element with the output value
+    outputIds.forEach(function(outputId) {
+      var outputElement = document.getElementById(outputId);
+      outputElement.innerHTML = output;
+    });
+  });
+}
+
+function getOutputIds(inputId) {
+  // Define mapping of input IDs to output IDs
+  var outputIdsMap = {
+    "Qno": ["vmob", "tmob", "Jno"],
+    "Qmob": ["umob", "smob"],
+    "Lper": ["Cper", "Dper", "Eper"]
+    // Add more mappings as needed
+  };
+
+  // Return the corresponding output IDs for the given input ID
+  return outputIdsMap[inputId] || [];
+}
+
+function limitInputLength(input, maxLength) {
+  if (input.value.length > maxLength) {
+    input.value = input.value.slice(0, maxLength);
+  }
+}
+window.onload = function() {
+    document.getElementById("convert").addEventListener("click", convertIt);
+
+    function convertIt() {
+        var input = document.getElementById("tcsh").value;
+        var output = convertToNepaliText(input);
+        var output2 = convertToNepaliNumber(input);
+
+        // Show and update the output in separate containers
+        var outputElement = document.getElementById("cashDetail");
+        var output2Element = document.getElementById("output2");
+        var output3Element = document.getElementById("output3");
+        var output4Element = document.getElementById("output4");
+        var output5Element = document.getElementById("output5");
+        var cashDetail2Element = document.getElementById("cashDetail2");
+        var cashDetail3Element = document.getElementById("cashDetail3");
+        var cashDetail4Element = document.getElementById("cashDetail4");
+
+        outputElement.innerHTML = output;
+        output2Element.innerHTML = convertToCommaNumber(output2);
+
+        // Duplicate the value of output2 to more containers (output3)
+        output3Element.innerHTML = convertToCommaNumber(output2);
+        output4Element.innerHTML = convertToCommaNumber(output2);
+        output5Element.innerHTML = convertToCommaNumber(output2);
+
+        // Duplicate the value of output2 to more containers (cashDetail2 to cashDetail4)
+        cashDetail2Element.innerHTML = (output);
+        cashDetail3Element.innerHTML = (output);
+        cashDetail4Element.innerHTML = (output);
+    }
+}
+function convertKarja(number) {
+    // ... your existing code for handling the click ...
+
+    if (number === 1) {
+      // Do something when "कर्जाको" is clicked
+      // For example, hide the "karjaSection" div
+      document.getElementById("karjaSection").style.display = "none";
+    } else if (number === 2) {
+      // Do something when "आवश्यकता" is clicked
+      // For example, alert a message
+      alert("के तपाइ कर्जा Amount Modify or Edit गर्न चाहनुहुन्छ।");
+
+      // Show the "karjaSection" div
+      document.getElementById("karjaSection").style.display = "block";
+    } else if (number === 3) {
+      document.getElementById("karjaSlash").style.display = "none";
+  }  
+  }
+document.addEventListener('DOMContentLoaded', function () {
+    const handleInputChange = (element, target) => {
+      element.addEventListener('change', () => {
+        target.innerHTML = element.value;
+      });
+    };
+  
+    const loanWork = document.getElementById('loanWork');
+    const lwtype = document.getElementById('lwtype');
+    handleInputChange(loanWork, lwtype);
+  
+    const ltype = document.getElementById('ltype');
+    const loantype = document.getElementById('loantype');
+    const loan1type = document.getElementById('loan1type');
+    handleInputChange(ltype, loantype);
+    handleInputChange(ltype, loan1type);
+  });
+  
+  $(document).ready(function () {
+    var datePickerOptions1 = {
+        dateFormat: "%y/%m/%d",
+        closeOnDateSelect: true,
+        defaultDate: "20, 07 20, 2020"
+    };
+
+    var datePickerOptions2 = {
+        dateFormat: "%y साल %M महिना %d गते %Dबार",
+        closeOnDateSelect: true,
+        defaultDate: "20, 07 20, 2020"
+    };
+
+    var datePickerOptions3 = {
+        dateFormat: "%y/%m/%d",
+        closeOnDateSelect: true,
+        defaultDate: "20, 07 20, 2020"
+    };
+
+    var datePickerOptions4 = {
+        dateFormat: "%y साल %M महिना %d गते %Dबार",
+        closeOnDateSelect: true,
+        defaultDate: "20, 07 20, 2020"
+    };
+    $("#date1").nepaliDatePicker(datePickerOptions1);
+    $("#date2").nepaliDatePicker(datePickerOptions2);
+    $("#date3").nepaliDatePicker(datePickerOptions3);
+    $("#date4").nepaliDatePicker(datePickerOptions4);
+
+    $("#convert1").on("click", function () {
+        var selectedDate = $("#date1").val();
+        $("#tdate, #t1date, #exdate, #ex1date").text(selectedDate);
+    });
+    $("#convert2").on("click", function () {
+        var selectedDate = $("#date2").val();
+        $("#tfdate, #t1fdate, #efxdate").text(selectedDate);
+    });
+    $("#convert3").on("click", function () {
+        var selectedDate = $("#date3").val();
+        $("#t2date, #x2date, #e2date").text(selectedDate);
+    });
+    $("#convert4").on("click", function () {
+        var selectedDate = $("#date4").val();
+        $("#tf2date, #y2date, #ef2xdate").text(selectedDate);
+    });
+});
+
+document.getElementById("convert").addEventListener("click", convertIt);
+
+    function convertIt() {
+      // List of input IDs to process
+      var inputIds = ["Qno", "aQmob", "Lper", "aLper"];
+      
+      // Loop through each input ID
+      inputIds.forEach(function(inputId) {
+        var input = document.getElementById(inputId).value;
+        var outputIds = getOutputIds(inputId);
+    var output = convertToNepaliNumber(input);
+        // var output = input; // Just echoing the input for demonstration purposes
+        // Update each output element with the output value
+        outputIds.forEach(function(outputId) {
+          // var outputElement
+
+          var outputElement = document.getElementById(outputId);
+          outputElement.innerHTML = output;
+        });
+      });
+    }
+
+    function getOutputIds(inputId) {
+  // Define mapping of input IDs to output IDs
+  var outputIdsMap = {
+    "Qno": ["vmob", "tmob", "Jno"],
+    "aQmob": ["umob", "smob"], // Corrected the output IDs
+    "Lper": ["Cper", "Dper", "Eper"],
+    "aLper": ["aCper", "aDper", "aEper"],
+    // Add more mappings as needed
+  };
+  function convertToNepaliNumber(input) {
+  return input; // Simply echoing the input for demonstration purposes
+}
+
+
+  // Return the corresponding output IDs for the given input ID
+  return outputIdsMap[inputId] || [];
+}
+    function limitInputLength(input, maxLength) {
+      if (input.value.length > maxLength) {
+        input.value = input.value.slice(0, maxLength);
+      }
+    }
+
+    
+  
---
+++ jserialscript.js
@@ -0,0 +1,379 @@
+$(document).ready(function(){
+    $('#loanWork').change(function(){
+        let gRmR_01 = $(this).val();
+        $('#lwtype').html(gRmR_01);
+    });
+
+    $('#ltype').change(function(){
+        let gRmR_02 = $(this).val();
+        $('#loantype,#loan1type').html(gRmR_02);
+    });
+
+    $('#fname').change(function(){
+        let gRmR_03 = $(this).val();
+        $('#fullName,#full1Name,#full2Name,#full3Name').html(gRmR_03);
+    });
+
+    $('#myage').change(function(){
+        let gRmR_04 = $(this).val();
+        $('#myAge,#my1Age').html(gRmR_04);
+    });
+
+    $('#pdist').change(function(){
+        let gRmR_05 = $(this).val();
+        $('#pDist,#p1Dist,#p2Dist').html(gRmR_05);
+    });
+
+    $('#tdist').change(function(){
+        let gRmR_06 = $(this).val();
+        $('#tDist,#t1Dist,#t2Dist').html(gRmR_06);
+    });
+
+    $('#pgapa').change(function(){
+        let gRmR_07 = $(this).val();
+        $('#pGapa,#p1Gapa,#p2Gapa').html(gRmR_07);
+    });
+
+    $('#pogapa').change(function(){
+        let gRmR_08 = $(this).val();
+        $('#toGapa,#to1Gapa,#to2Gapa').html(gRmR_08);
+    });
+    $('#pogapaA').change(function(){
+        let gRmRz_08 = $(this).val();
+        $('#poGapa,#po1Gapa').html(gRmRz_08);
+    });
+
+    $('#pward').change(function(){
+        let gRmR_09 = $(this).val();
+        $('#pWard,#p1Ward,#p2Ward').html(gRmR_09);
+    });
+
+    $('#tgapaz').change(function(){
+        let gRmR_10 = $(this).val();
+        $('#tGapa,#t1Gapa,#t2Gapa').html(gRmR_10);
+    });
+
+    $('#togapa').change(function(){
+        let gRmR_11 = $(this).val();
+        $('#toGapa,#to1Gapa,#to2Gapa').html(gRmR_11);
+    });
+
+    $('#tward').change(function(){
+        let gRmR_12 = $(this).val();
+        $('#tWard,#t1Ward,#t2Ward').html(gRmR_12);
+    });
+
+    $('#grandpa').change(function(){
+        let gRmR_13 = $(this).val();
+        $('#grandPa,#grand1Pa,#grand2Pa').html(gRmR_13);
+    });
+
+    $('#father').change(function(){
+        let gRmR_14 = $(this).val();
+        $('#Father,#Father1,#Father2').html(gRmR_14);
+    });
+
+    $('#wifehub').change(function(){
+        let gRmR_15 = $(this).val();
+        $('#wifeHub,#wife1Hub,#wife2Hub').html(gRmR_15);
+    });
+
+    $('#todaydate').change(function(){
+        let gRmR_16 = $(this).val();
+        $('#todayDate,#today1Date,#today2Date').html(gRmR_16);
+    });
+
+    $('#mob').change(function(){
+        let gRmR_17 = $(this).val();
+        $('#Mob').html(gRmR_17);
+    });
+
+    $('#mrno').change(function(){
+        let gRmR_18 = $(this).val();
+        $('#mrNo').html(gRmR_18);
+    });
+
+    $('#mrcr').change(function(){
+        let gRmR_19 = $(this).val();
+        $('#mr2cr').html(gRmR_19);
+    });
+
+    $('#intPerc').change(function(){
+        let gRmR_20 = $(this).val();
+        $('#PercInt').html(gRmR_20);
+    });
+
+    $('#paidyear').change(function(){
+        let gRmR_21 = $(this).val();
+        $('#paidYear').html(gRmR_21);
+    });
+
+    $('#paidmonth').change(function(){
+        let gRmR_22 = $(this).val();
+        $('#paidMonth').html(gRmR_22);
+    });
+
+    $('#paidday').change(function(){
+        let gRmR_23 = $(this).val();
+        $('#paidDay').html(gRmR_23);
+    });
+
+    $('#mperiodtype').change(function(){
+        let gRmR_24 = $(this).val();
+        $('#mPeriodType').html(gRmR_24);
+    });
+
+    $('#memo21').change(function(){
+        let gRmR_25 = $(this).val();
+        $('#Ememo21, #gMemo18').html(gRmR_25);
+    });
+    
+    $('#memoz21').change(function(){
+        let gRmRz_25 = $(this).val();
+        $('#gMemo1').html(gRmRz_25);
+    });
+
+    $('#memo27').change(function(){
+        let gRmR_26 = $(this).val();
+        $('#Ememo27,#gMemo24').html(gRmR_26);
+    });
+    $('#memoz27').change(function(){
+        let gRmRz_26 = $(this).val();
+        $('#gMemo2').html(gRmRz_26);
+    });
+
+    $('#memo33').change(function(){
+        let gRmR_27 = $(this).val();
+        $('#Ememo33,#gMemo28').html(gRmR_27);
+    });
+    $('#memoz33').change(function(){
+        let gRmRz_27 = $(this).val();
+        $('#gMemo3').html(gRmRz_27);
+    });
+
+    $('#memo22a').change(function(){
+        let gRmR_28 = $(this).val();
+        $('#Ememo22,#gMemo11').html(gRmR_28);
+    });
+
+    $('#memo28a').change(function(){
+        let gRmR_29 = $(this).val();
+        $('#Ememo28,#gMemo12').html(gRmR_29);
+    });
+
+    $('#memo34a').change(function(){
+        let gRmR_30 = $(this).val();
+        $('#Ememo34,#gMemo19').html(gRmR_30);
+    });
+
+    $('#memo22b').change(function(){
+        let gRmR_31 = $(this).val();
+        $('#gMemo13').html(gRmR_31);
+    });
+
+    $('#memo28b').change(function(){
+        let gRmR_32 = $(this).val();
+        $('#gMemo14').html(gRmR_32);
+    });
+
+    $('#memo34b').change(function(){
+        let gRmR_33 = $(this).val();
+        $('#gMemo20').html(gRmR_33);
+    });
+
+    $('#togapa1').change(function(){
+        let gRmR_34 = $(this).val();
+        $('#togapa7').html(gRmR_34);
+    });
+
+    $('#togapa5').change(function(){
+        let gRmR_35 = $(this).val();
+        $('#togapa8').html(gRmR_35);
+    });
+
+    $('#togapa6').change(function(){
+        let gRmR_36 = $(this).val();
+        $('#togapa9').html(gRmR_36);
+    });
+
+    $('#togapa2').change(function(){
+        let gRmR_37 = $(this).val();
+        $('#gMemo15').html(gRmR_37);
+    });
+
+    $('#togapa3').change(function(){
+        let gRmR_38 = $(this).val();
+        $('#gMemo21').html(gRmR_38);
+    });
+
+    $('#togapa4').change(function(){
+        let gRmR_39 = $(this).val();
+        $('#gMemo25').html(gRmR_39);
+    });
+
+    $('#memo22d').change(function(){
+        let gRmR_40 = $(this).val();
+        $('#gMemo16').html(gRmR_40);
+    });
+
+    $('#memo28d').change(function(){
+        let gRmR_41 = $(this).val();
+        $('#gMemo22').html(gRmR_41);
+    });
+
+    $('#memo34d').change(function(){
+        let gRmR_42 = $(this).val();
+        $('#gMemo26').html(gRmR_42);
+    });
+
+    $('#memo23').change(function(){
+        let gRmR_43 = $(this).val();
+        $('#Ememo23').html(gRmR_43);
+    });
+    $('#memoz23').change(function(){
+        let gRmRz_43 = $(this).val();
+        $('#gMemo7').html(gRmRz_43);
+    });
+
+    $('#memo29').change(function(){
+        let gRmR_44 = $(this).val();
+        $('#Ememo29').html(gRmR_44);
+    });
+
+    $('#memoz29').change(function(){
+        let gRmRz_44 = $(this).val();
+        $('#gMemo8').html(gRmRz_44);
+    });
+
+    $('#memo35').change(function(){
+        let gRmR_45 = $(this).val();
+        $('#Ememo35, #gMemo9').html(gRmR_45);
+    });
+
+    $('#memoz35').change(function(){
+        let gRmRz_45 = $(this).val();
+        $('#gMemo9').html(gRmRz_45);
+    });
+
+    $('#memo24').change(function(){
+        let gRmR_46 = $(this).val();
+        $('#Ememo24').html(gRmR_46);
+    });
+
+    $('#memo30').change(function(){
+        let gRmR_47 = $(this).val();
+        $('#Ememo30').html(gRmR_47);
+    });
+
+    $('#memo36').change(function(){
+        let gRmR_48 = $(this).val();
+        $('#Ememo36').html(gRmR_48);
+    });
+
+    $('#memo25').change(function(){
+        let gRmR_49 = $(this).val();
+        $('#Ememo25').html(gRmR_49);
+    });
+
+    $('#memo31').change(function(){
+        let gRmR_50 = $(this).val();
+        $('#Ememo31').html(gRmR_50);
+    });
+
+    $('#memo37').change(function(){
+        let gRmR_51 = $(this).val();
+        $('#Ememo37').html(gRmR_51);
+    });
+
+    $('#todayyear').change(function(){
+        let gRmR_52 = $(this).val();
+        $('#TodayYear').html(gRmR_52);
+    });
+
+    $('#todaymonth').change(function(){
+        let gRmR_53 = $(this).val();
+        $('#TodayMonth').html(gRmR_53);
+    });
+
+    $('#todayday').change(function(){
+        let gRmR_54 = $(this).val();
+        $('#TodayDay').html(gRmR_54);
+    });
+
+    $('#weekday').change(function(){
+        let gRmR_55 = $(this).val();
+        $('#TodayWeekDay').html(gRmR_55);
+    });
+   
+
+
+    $('#memoz22a').change(function(){
+        let gRmR_56= $(this).val();
+        $('#gMemoz11').html(gRmR_56);
+    });
+    $('#memoz22b').change(function(){
+        let gRmR_57= $(this).val();
+        $('#gMemoz13').html(gRmR_57);
+    });
+    $('#togapaz1').change(function(){
+        let gRmR_58= $(this).val();
+        $('#togapaz7').html(gRmR_58);
+    });
+    $('#togapaz2').change(function(){
+        let gRmR_59= $(this).val();
+        $('#gMemoz15').html(gRmR_59);
+    });
+    $('#memoz22d').change(function(){
+        let gRmR_60= $(this).val();
+        $('#gMemoz16').html(gRmR_60);
+    });
+
+
+
+
+
+    
+    $('#memoz28a').change(function(){
+        let gRmR_61= $(this).val();
+        $('#gMemozs11').html(gRmR_61);
+    });
+    $('#memoz28b').change(function(){
+        let gRmR_62= $(this).val();
+        $('#gMemozs13').html(gRmR_62);
+    });
+    $('#togapaz5').change(function(){
+        let gRmR_63= $(this).val();
+        $('#togapazs7').html(gRmR_63);
+    });
+    $('#togapaz3').change(function(){
+        let gRmR_64= $(this).val();
+        $('#gMemozs15').html(gRmR_64);
+    });
+    $('#memoz28d').change(function(){
+        let gRmR_65= $(this).val();
+        $('#gMemozs16').html(gRmR_65);
+    });
+
+
+    $('#memoz34a').change(function(){
+        let gRmR_66= $(this).val();
+        $('#gMemozp11').html(gRmR_66);
+    });
+    $('#memoz34b').change(function(){
+        let gRmR_67= $(this).val();
+        $('#gMemozp13').html(gRmR_67);
+    });
+    $('#togapaz6').change(function(){
+        let gRmR_68= $(this).val();
+        $('#togapazp7').html(gRmR_68);
+    });
+    $('#togapaz4').change(function(){
+        let gRmR_69= $(this).val();
+        $('#gMemozp15').html(gRmR_69);
+    });
+    $('#memoz34d').change(function(){
+        let gRmR_70= $(this).val();
+        $('#gMemozp16').html(gRmR_70);
+    });
+
+});
